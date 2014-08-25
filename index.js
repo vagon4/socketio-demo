7a116dce5e37db9df93bfbb74351fb71c95112e2
Vaughan Ramsey <vagon4@gmail.com>
2014-08-25 10:48:36 +0000

    added some emit to all users functionality and corrected a css typo on #messages li:nth-child

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ io.on('connection', function(socket){
 	console.log('a user connected');
 	socket.on('chat message',function(msg){
 		console.log('message: ' + msg);
+		io.emit('chat message', msg);
 	});
 	socket.on('disconnect',function(){
 		console.log('user disconnected');
